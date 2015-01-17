de0e160e255df36d53129e3fad24f9835daa6607
Brandon Powell <bpowell29a@gmail.com>
2015-01-17 13:01:37 -0500

    Items now have color

--- Images/app/src/main/java/edu/oakland/images/models/Item.java
+++ Images/app/src/main/java/edu/oakland/images/models/Item.java
@@ -1,5 +1,9 @@
 package edu.oakland.images.models;
 
+import java.util.ArrayList;
+
+import edu.oakland.images.processing.ColorInfo;
+
 /**
  * Created by brandon on 1/17/15.
  */
@@ -7,12 +11,25 @@ public class Item {
     public String name;
     public ClothingType clothingType;
     public ArticleType articleType;
+    public ArrayList<ColorInfo> colors;
 
-    public Item(String name, ClothingType clothingType, ArticleType articleType) {
+    public Item(String name, ClothingType clothingType, ArticleType articleType, ArrayList<ColorInfo> colors) {
         this.name = name;
         this.clothingType = clothingType;
         this.articleType = articleType;
+        this.colors = colors;
     }
 
-    public Item() {}
+    public Item(String name, ClothingType clothingType, ArticleType articleType, ColorInfo color) {
+        ArrayList<ColorInfo> colors = new ArrayList<>();
+        colors.add(color);
+        this.name = name;
+        this.clothingType = clothingType;
+        this.articleType = articleType;
+        this.colors = colors;
+    }
+
+    public Item() {
+        this.colors = new ArrayList<>();
+    }
 }
