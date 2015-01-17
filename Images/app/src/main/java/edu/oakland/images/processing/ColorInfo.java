22d0231264b4a9e782a2f77920961d72711ce764
Brandon Powell <bpowell29a@gmail.com>
2015-01-17 13:08:18 -0500

    Forgot to remove reference to deleted class

--- Images/app/src/main/java/edu/oakland/images/processing/ColorInfo.java
+++ Images/app/src/main/java/edu/oakland/images/processing/ColorInfo.java
@@ -4,13 +4,11 @@ package edu.oakland.images.processing;
  * Created by brandon on 1/17/15.
  */
 public class ColorInfo {
-    public String color;
     public int value;
     public int size;
 
     public ColorInfo(int value, int size) {
         this.value = value;
-        this.color = ColorUtils.whatColor(value);
         this.size = size;
     }
 }
