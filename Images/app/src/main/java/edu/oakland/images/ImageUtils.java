0dab2b272391aebbf9dbb5bb462fcdce1ae87eb9
Brandon Powell <bpowell29a@gmail.com>
2015-01-17 05:44:43 -0500

    Computes most used color in image.
    Base layout to grab most used color in an image. Scales the image by
    some factor then removes a percentage around the border. This removes
    part of the background.

---
+++ Images/app/src/main/java/edu/oakland/images/ImageUtils.java
@@ -0,0 +1,56 @@
+package edu.oakland.images;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by brandon on 1/17/15.
+ */
+public class ImageUtils {
+    public static final double PERCENT_TO_CUT = 0.10;
+    public static final int SHRINK_FACTOR = 2;
+
+    public static ColorInfo getTopColors(String placeholder) {
+        HashMap<Integer, Integer> usedColors = new HashMap<Integer, Integer>();
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = SHRINK_FACTOR;
+
+        Bitmap image = BitmapFactory.decodeFile(placeholder, opts);
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        int width_start = (int) (width * PERCENT_TO_CUT);
+        int height_start = (int) (height * PERCENT_TO_CUT);
+        int width_end = width - width_start;
+        int height_end = height - height_start;
+
+        for(int i=width_start; i<width_end; i++) {
+            for(int j=height_start; j<height_end; j++) {
+                int pixel = image.getPixel(i, j);
+                if (usedColors.containsKey(pixel)) {
+                    int t = usedColors.get(pixel) + 1;
+                    usedColors.put(pixel, t);
+                } else {
+                    usedColors.put(pixel, 1);
+                }
+            }
+        }
+
+        Iterator it = usedColors.entrySet().iterator();
+        ColorInfo topColor = new ColorInfo(0,0);
+
+        while (it.hasNext()) {
+            Map.Entry<Integer, Integer> pairs = (Map.Entry) it.next();
+            int size = pairs.getValue();
+            if (size > topColor.size) {
+                topColor = new ColorInfo(pairs.getKey(), size);
+            }
+        }
+
+        return topColor;
+    }
+}
