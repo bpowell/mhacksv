a98e4663d76e429d23261d05364ccb3446dffd74
swiggins83 <stevenwiggins83@gmail.com>
2015-01-17 04:27:15 -0500

    Working

---
+++ Images/app/src/main/java/edu/oakland/images/activities/MainActivity.java
@@ -0,0 +1,53 @@
+package edu.oakland.images.activities;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import edu.oakland.images.R;
+import edu.oakland.images.fragments.WhatToWearFragment_;
+
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setUpFragment();
+    }
+
+    private void setUpFragment() {
+        Fragment fragment = new WhatToWearFragment_();
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Images/app/src/main/java/edu/oakland/images/fragments/WhatToWearFragment.java
@@ -0,0 +1,14 @@
+package edu.oakland.images.fragments;
+
+import android.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import edu.oakland.images.R;
+
+/**
+ * Created by steven on 1/17/15.
+ */
+@EFragment(R.layout.fragment_what_to_wear)
+public class WhatToWearFragment extends Fragment {
+}
