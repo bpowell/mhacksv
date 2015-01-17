5bff3b264df828d86cafbf6d92c71db838b153a1
Brandon Powell <bpowell29a@gmail.com>
2015-01-17 17:14:20 -0500

    Added import statements

--- Images/app/src/main/java/edu/oakland/images/database/OutfitDataSource.java
+++ Images/app/src/main/java/edu/oakland/images/database/OutfitDataSource.java
@@ -1,9 +1,19 @@
 package edu.oakland.images.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import edu.oakland.images.models.ArticleType;
+import edu.oakland.images.models.ClothingType;
+import edu.oakland.images.models.Item;
+import edu.oakland.images.models.Outfit;
+import edu.oakland.images.processing.ColorInfo;
 
 /**
  * Created by brandon on 1/17/15.
