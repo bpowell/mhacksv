65f00b83dfd0bd1998cd784b48917b2f7702f298
Brandon Powell <bpowell29a@gmail.com>
2015-01-18 02:43:21 -0500

    a different way to look at the rules

--- Images/app/src/main/java/edu/oakland/images/style/Rules.java
+++ Images/app/src/main/java/edu/oakland/images/style/Rules.java
@@ -5,29 +5,41 @@ import java.util.Iterator;
 
 import edu.oakland.images.models.Item;
 import edu.oakland.images.models.Outfit;
+import edu.oakland.images.processing.ColorInfo;
+import edu.oakland.images.processing.ImageUtils;
 
 /**
  * Created by brandon on 1/17/15.
  */
 public class Rules {
-    public static boolean passRuleCheck(Outfit outfit) {
-        ArrayList<Item> items = outfit.items;
-        int size = items.size();
-
-        //If there is no or one item, nothing to compare against
-        if(size==0 || size==1) {
-            return true;
+    public static boolean passRuleCheck(Item item1, Item item2) {
+        if(item1.articleType == item2.articleType) {
+            return false;
         }
 
-        //Check for duplicate items
-        for(int i=0; i<size; i++) {
-            for(int j=i+1; j<size; j++) {
-                if(items.get(i).articleType == items.get(j).articleType) {
-                    return false;
+        ArrayList<ColorInfo> colorsItem1 = new ArrayList<>();
+        for(ColorInfo c : item1.colors) {
+            colorsItem1.add(ImageUtils.similarColor(String.format("%06X", c.value)));
+        }
+
+        ArrayList<ColorInfo> colorsItem2 = new ArrayList<>();
+        for(ColorInfo c: item2.colors) {
+            colorsItem2.add(ImageUtils.similarColor(String.format("%06X", c.value)));
+        }
+
+        //belt+shoe check
+        boolean shoebelt = false;
+        for(ColorInfo c1 : colorsItem1) {
+            for(ColorInfo c2 : colorsItem2) {
+                if(c1.value==c2.value) {
+                    shoebelt = true;
                 }
             }
         }
+        if(!shoebelt) {
+            return false;
+        }
 
-        return false;
+        return true;
     }
 }
