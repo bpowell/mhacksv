3bac930e3a5e68465b86e8e5197b6cca3f764b1e
swiggins83 <stevenwiggins83@gmail.com>
2015-01-17 20:49:24 -0500

    Base of UI for image processing

--- Images/app/src/main/java/edu/oakland/images/activities/AddArticleActivity.java
+++ Images/app/src/main/java/edu/oakland/images/activities/AddArticleActivity.java
@@ -1,12 +1,15 @@
 package edu.oakland.images.activities;
 
-import android.app.Activity;
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.support.v7.widget.Toolbar;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.ViewById;
 
 import edu.oakland.images.R;
 
@@ -14,10 +17,18 @@ import edu.oakland.images.R;
  * Created by steven on 1/17/15.
  */
 @EActivity(R.layout.activity_add_article)
-public class AddArticleActivity extends Activity {
+public class AddArticleActivity extends ActionBarActivity {
+
+    @ViewById(R.id.toolbar)
+    Toolbar toolbar;
 
     @Extra
     Uri uri;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @AfterViews
     void init() {
